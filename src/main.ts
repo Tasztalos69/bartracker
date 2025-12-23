406d774c5f4b125d5760e9821545aa907973f267
Kiss Benedek Máté <tasztalos69@gmail.com>
2025-12-23 20:44:32 +0100

    New analytics

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { VueFire, VueFireAuth } from "vuefire";
 import App from "./App.vue";
 import router from "./router";
+import Swetrix, { type SwetrixVuePluginOptions } from "swetrix-vue";
 
 import { firebaseApp } from "./firebase";
 
@@ -14,5 +15,14 @@ app.use(VueFire, {
   firebaseApp,
   modules: [VueFireAuth()],
 });
+
+const swetrixOpts: SwetrixVuePluginOptions = {
+  pid: "dgNwCz7RR0P4",
+  initOptions: {
+    apiURL: "https://analytics-api.bmk.dev/log",
+  },
+};
+
+app.use(Swetrix, swetrixOpts);
 
 app.mount("#app");
